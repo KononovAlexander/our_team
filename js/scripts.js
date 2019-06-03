e861af6d3624d8066fe206d5bb869d2a5105ea3c
FRHigh <kononov8214@gmail.com>
2019-06-03 18:05:33 +0300

    added slick-slider

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		
+		arrows: true,
+		prevArrow: '<a href="#" class="slider__arrow slider__arrow--left"></a>' ,
+        nextArrow: '<a href="#" class="slider__arrow slider__arrow--right"></a>' ,
+        dots: true,
+        dotsClass: 'slider__dots',
+        autoplay: false,
+
+
+
+	});
+});
